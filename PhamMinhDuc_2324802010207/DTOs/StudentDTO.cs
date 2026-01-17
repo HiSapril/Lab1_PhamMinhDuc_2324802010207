ee46bccada3bd1421134e8441c31f612ef99f96e
HiSapril <2324802010207@student.tdmu.edu.vn>
2026-01-17 17:20:53 +0700

    Initial commit: Student API with CRUD and SQL Server

---
+++ PhamMinhDuc_2324802010207/DTOs/StudentDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PhamMinhDuc_2324802010207.DTOs
+{
+    public class StudentDTO
+    {
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+
+    }
+}
---
+++ PhamMinhDuc_2324802010207/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace PhamMinhDuc_2324802010207.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
